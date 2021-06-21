39b395c32b5eec892be7cdc288d2facfe7034132
Joseph C <43707388+Trip1eLift@users.noreply.github.com>
2021-06-22 00:33:11 +0800

    Add files via upload

--- python/face_landmarks.py
+++ python/face_landmarks.py
@@ -13,7 +13,7 @@ def face_mesh_trace():
 
     mpDraw = mp.solutions.drawing_utils
     mpFaceMesh = mp.solutions.face_mesh
-    faceMesh = mpFaceMesh.FaceMesh(max_num_faces=1)
+    faceMesh = mpFaceMesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_tracking_confidence=0.6, min_detection_confidence=0.6)
     drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
 
     pTime = 0
@@ -28,6 +28,15 @@ def face_mesh_trace():
         results = faceMesh.process(frameRGB)
         if results.multi_face_landmarks:
             for faceLms in results.multi_face_landmarks:
+                #print(faceLms.landmark[0]) # landmark includes x, y, z
+                """
+                x, y and z. x and y are normalized to [0.0, 1.0] by the 
+                image width and height respectively. z represents the 
+                landmark depth with the depth at center of the head being 
+                the origin, and the smaller the value the closer the 
+                landmark is to the camera. The magnitude of z uses roughly 
+                the same scale as x
+                """
                 mpDraw.draw_landmarks(frame, faceLms, mpFaceMesh.FACE_CONNECTIONS,
                                       drawSpec, drawSpec)
 
